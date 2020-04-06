79c19bce8137547dee8d92d744600396c6875393
Max Zakzook <mzakzook@me.com>
2020-04-05 17:04:39 -0700

    Create landing page component

---
+++ src/LandingPage.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+
+class LandingPage extends React.Component {
+    // state = {
+    // }
+
+
+
+    render() {
+        return (
+            <div>
+                
+                <div >
+                    <div >
+                        
+                    </div>
+                    
+                    <div>
+                        
+                   </div>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default LandingPage 
